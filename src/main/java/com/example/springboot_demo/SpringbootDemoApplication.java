b770f0953d562423e514765598279c8e28166670
yuvraj <bhatiayuvraj98@gmail.com>
2026-04-08 22:32:37 +0530

    Initial commit

---
+++ src/main/java/com/example/springboot_demo/SpringbootDemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.springboot_demo;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringbootDemoApplication {
+
+	public static void main(String[] args) {
+
+		ApplicationContext contxt=SpringApplication.run(SpringbootDemoApplication.class, args);
+		System.out.println("helo world");
+		Alien alien =  contxt.getBean(Alien.class);
+		alien.code();
+
+
+	}
+
+}
